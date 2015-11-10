eee193a5bc3d6022cd6fc6fe9b66e0b0a83b01f7
Alex Coto <alexrc09@gmail.com>
2015-11-10 11:12:36 -0500

    Cleanup and beautify some static assets

--- static/js/map.js
+++ static/js/map.js
@@ -1,13 +1,6 @@
-// function rotate (theta, x) {
-//     // cos sin
-//     // -sin cos
-//     return [Math.cos(theta) * x.lat + Math.sin(theta) * x.lng,
-//            -Math.sin(theta) * x.lat + Math.cos(theta) * x.lng];
-// }
-
 var map = L.map('map').setView([0, 0], 5);
 
-var url = "assets/images/LTL_min1.png";
+var url = "static/images/LTL_min1.png";
 var imagebounds = [
     [-10, -10],
     [10, 22]
@@ -15,22 +8,22 @@ var imagebounds = [
 var imageOverlay = L.imageOverlay(url, imagebounds).addTo(map);
 
 var plottedPolyline = L.Polyline.Plotter([
-        [0,6]        
-    ],{
-        weight: 5,
-		readOnly: true,		
-    }).addTo(map);
+    [0, 6]
+], {
+    weight: 5,
+    readOnly: true,
+}).addTo(map);
 
 //function plot(enable){
 //	if(enable)plottedPolyline.
 //}
 
 var readOnly = true;
-$("#plot").click(function(){
-        readOnly = !readOnly;
-        plottedPolyline.setReadOnly(readOnly);
+$("#plot").click(function() {
+    readOnly = !readOnly;
+    plottedPolyline.setReadOnly(readOnly);
 });
-	
+
 L.DomEvent.on(imageOverlay._image, 'mouseenter', function(e) {
     $("#robot-logo").css("visibility", "visible");
     $("#video-streaming").css("visibility", "visible");
--- static/js/ui-actions.js
+++ static/js/ui-actions.js
@@ -1,25 +1,25 @@
-function toggleOverlay(mode){
-	$("#overlay").toggleClass("visible", mode);
+function toggleOverlay(mode) {
+    $("#overlay").toggleClass("visible", mode);
 }
 
-$(".icon#settings").click(function(){
-	$("#sidebar-left").toggleClass("expanded");
-	toggleOverlay($("#sidebar-left").hasClass("expanded"));
+$(".icon#settings").click(function() {
+    $("#sidebar-left").toggleClass("expanded");
+    toggleOverlay($("#sidebar-left").hasClass("expanded"));
 });
 
-$("#overlay").click(function(){
-	toggleOverlay(false);
-	$("#sidebar-left").removeClass("expanded");
+$("#overlay").click(function() {
+    toggleOverlay(false);
+    $("#sidebar-left").removeClass("expanded");
 });
 
-imageOverlay.on("mouseover", function(){
-	$("#robot-logo").css("visibility", "visible");
-	$("#video-streaming").css("visibility", "visible");
-	$("#video-streaming").attr("src", "http://10.0.0.1:8080/stream/video.mjpeg")
+imageOverlay.on("mouseover", function() {
+    $("#robot-logo").css("visibility", "visible");
+    $("#video-streaming").css("visibility", "visible");
+    $("#video-streaming").attr("src", "http://10.0.0.1:8080/stream/video.mjpeg")
 });
 
-imageOverlay.on("mouseout", function(){
-	$("#robot-logo").css("visibility", "hidden");
-	$("#video-streaming").css("visibility", "hidden");
-	$("#video-streaming").attr("src", null);
+imageOverlay.on("mouseout", function() {
+    $("#robot-logo").css("visibility", "hidden");
+    $("#video-streaming").css("visibility", "hidden");
+    $("#video-streaming").attr("src", null);
 });
