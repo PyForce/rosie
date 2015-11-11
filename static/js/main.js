7dfba94427604a551d35ba69a9ac7168f54922bf
Alex Coto <alexrc09@gmail.com>
2015-11-10 20:22:54 -0500

    Set selected mode button background and fix path building

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,13 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> bfdbab2... Set selected mode button background and fix path building
 $(document).ready(function(){
 		// MENU
 		$('#i-menu').click(function(){
 			$('#settings').hide();
       $('#menu').toggle();
+<<<<<<< HEAD
     });
 
 		// SETTINGS
@@ -65,41 +69,61 @@ $(document).ready(function(){
 	$('#i-menu').click(function(){
 		$('#settings').hide();
         $('#menu').toggle();
+=======
+>>>>>>> bfdbab2... Set selected mode button background and fix path building
     });
-	
-	// SETTINGS
-	$('#i-settings').click(function(){
-		$('#menu').hide();
-        $('#settings').toggle();
+
+		// SETTINGS
+		$('#i-settings').click(function(){
+			$('#menu').hide();
+      $('#settings').toggle();
     });
-	
-	// TOGGLE MENU/SETTINGS
+
+		// TOGGLE MENU/SETTINGS
     $('.list-menu a').click(function(){
         $('.list-menu').hide();
     });
-	
-	// MODE (TEXT)
-	$('#m-item1, #i-text').click(function(){
-        $('#mode-text').toggle(true);
-		$("#video-streaming").css("margin-bottom", "60px");
-    });
-	
-	// KEY
-	$('#m-item2, #i-key').click(function(){
-        $('#mode-text').hide();
-		$("#video-streaming").css("margin-bottom", "10px");
-    });
-	
-	// P2P
-	$('#m-item3, #i-p2p').click(function(){
-        $('#mode-text').hide();
-		$("#video-streaming").css("margin-bottom", "10px");
+
+		// MODE (TEXT)
+		$('#m-item1, #i-text').click(function(){
+      $('#mode-text').toggle(true);
+			$("#video-streaming").css("margin-bottom", "60px");
     });
 
-	// CAMERA
-	$('#m-item4').click(function(){
-		$('#video-streaming').toggle();
+		// KEY
+		$('#m-item2, #i-key').click(function(){
+      $('#mode-text').hide();
+			$("#video-streaming").css("margin-bottom", "10px");
     });
+
+		// P2P
+		$('#m-item3, #i-p2p').click(function(){
+      $('#mode-text').hide();
+			$("#video-streaming").css("margin-bottom", "10px");
+    });
+
+		// CAMERA
+		$('#m-item4').click(function(){
+			$('#video-streaming').toggle();
+    });
+<<<<<<< HEAD
 	
 })(window, jQuery);
 >>>>>>> 7207f9e... Mega commit
+=======
+
+		$('.icon').click(function(){
+			$('.icon').removeClass('active');
+			$(this).addClass('active');
+		});
+});
+
+//
+// (function (window, $) {
+// 	'use strict';
+//
+// 	// Cache document for fast access.
+// 	var document = window.document;
+//
+// })(window, jQuery);
+>>>>>>> bfdbab2... Set selected mode button background and fix path building
