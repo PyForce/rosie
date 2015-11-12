f1ad2cb847ba20bf95917d784f76c0109098b244
Gustavo Viera López <gvieralopez@gmail.com>
2015-11-11 20:11:44 -0800

    Try to fix the TypeError on function run() for argument host

--- modules/WebHUD/manage.py
+++ modules/WebHUD/manage.py
@@ -6,13 +6,13 @@ from WebHUD import app  #, sio
 from WebHUD.restapi import *
 from WebHUD.views import *
 
-host = '0.0.0.0'
-port = 5000
+myhost = '0.0.0.0'
+myport = 5000
 debug = False
 use_reloader = True
 
 def run_server():
-    app.run(app, host=host, port=port, debug=debug, use_reloader=use_reloader)
+    app.run(app, host=myhost, port=myport, debug=debug, use_reloader=use_reloader)
     #sio.run(app, host=host, port=port, debug=debug, use_reloader=use_reloader)
 
 
