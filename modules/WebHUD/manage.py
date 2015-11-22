e862ecf04a09466fef8039c4e70c6776c3574389
Antonio Serrano Muñoz <toni.sm91@gmail.com>
2015-11-22 17:45:44 -0500

    Merge branch 'master' of 10.0.0.1:rOSi

--- modules/WebHUD/manage.py
+++ modules/WebHUD/manage.py
@@ -9,7 +9,7 @@ from WebHUD.views import *
 host = '0.0.0.0'
 port = 5000
 debug = False
-use_reloader = True
+use_reloader = False
 
 def run_server():
     sio.run(app, host=host, port=port, debug=debug, use_reloader=use_reloader)
