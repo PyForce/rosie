26179ac321ee2041a961be6b5c6d0003b84e667d
Rolando Urquiza <rolurquiza@gmail.com>
2015-11-25 19:59:35 -0500

    Add submit to ordex form

--- modules/WebHUD/static/js/main.js
+++ modules/WebHUD/static/js/main.js
@@ -28,8 +28,8 @@ $(document).ready(function() {
     })
 
     $("#form-text").submit(function(event) {
-        event.preventDefault();
         setText(undefined, $("#text-order").val());
+        event.preventDefault();
     });
 
     // // KEY
