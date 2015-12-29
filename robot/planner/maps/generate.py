9d8c07045c42f72777eb8ebaae998b5c35a9d872
Fernando Martínez González <frndmartinezglez@gmail.com>
2015-12-29 18:26:15 -0500

    Graph points that will be expanded
    Graph using matplotlib just for test

--- robot/planner/maps/generate.py
+++ robot/planner/maps/generate.py
@@ -2,15 +2,17 @@
 import json
 from collections import namedtuple
 
+import matplotlib.pyplot as plt
+
 
 Point = namedtuple('Point', ['x', 'y', 'room'])
 
-def get_all_wall_points(rooms):
-    points = []
+def get_all_points(rooms):
+    points = [[]]
     for r in rooms.items():
-        for ps in r[1]['walls']['geometry']['coordinates']:
-            for p in ps:
-                points.append(Point(p[0], p[1], r[0]))
+        for ps in r[1]['borders']['geometry']['coordinates']:
+            points[-1] += [Point(p[0], p[1], r[0]) for p in ps]
+        points.append([])
     return points
 
 
@@ -19,7 +21,14 @@ def generate(jsonfile):
     with open(jsonfile) as f:
         rooms_map = json.load(f)
     if rooms_map:
-        allwps = get_all_wall_points(rooms_map['rooms'])
-        print(allwps)
+        allps = get_all_points(rooms_map['rooms'])
+        print(allps)
+        for wps in allps:
+            x = [p.x for p in wps]
+            y = [p.y for p in wps]
+            plt.plot(x, y)
+        plt.xlim([-1, 9.2])
+        plt.ylim([-1, 3.2])
+        plt.show()
 
 generate('map.json')
