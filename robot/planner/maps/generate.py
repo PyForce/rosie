77592883f181087a46b36d5071c533034c7d296e
Fernando Martínez González <frndmartinezglez@gmail.com>
2015-12-30 19:22:46 -0500

    Refactorize matplotlib's paint

--- robot/planner/maps/generate.py
+++ robot/planner/maps/generate.py
@@ -7,6 +7,11 @@ import matplotlib.pyplot as plt
 
 Point = namedtuple('Point', ['x', 'y', 'room'])
 
+def calculate_suport_points(points):
+    for ps in points:
+        pass
+
+
 def get_all_points(rooms):
     points = [[]]
     for r in rooms.items():
@@ -16,19 +21,25 @@ def get_all_points(rooms):
     return points
 
 
+def paint(allps):
+    for wps in allps:
+        x = [p.x for p in wps]
+        y = [p.y for p in wps]
+        plt.plot(x, y)
+    plt.xlim([-1, 9.2])
+    plt.ylim([-1, 3.2])
+    plt.show()
+
 def generate(jsonfile):
     rooms_map = None
     with open(jsonfile) as f:
         rooms_map = json.load(f)
     if rooms_map:
+        # Obtain a list which has for every room
+        # the points of it's bounds
         allps = get_all_points(rooms_map['rooms'])
-        print(allps)
-        for wps in allps:
-            x = [p.x for p in wps]
-            y = [p.y for p in wps]
-            plt.plot(x, y)
-        plt.xlim([-1, 9.2])
-        plt.ylim([-1, 3.2])
-        plt.show()
+
+        paint(allps)
+
 
 generate('map.json')
