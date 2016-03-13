ea7f05010ac33c710fab076117b1b901ba0bf605
Fernando Martínez González <frndmartinezglez@gmail.com>
2016-03-13 19:59:17 -0400

    Calculate intersection
    Well done for counter-clock-wise direction

--- robot/planner/maps/generate.py
+++ robot/planner/maps/generate.py
@@ -1,11 +1,12 @@
 # from robot.planner.maps.graph import AdjacencyMatrixGraph as Graph
 import json
 from collections import namedtuple
+import numpy as np
 
 import matplotlib.pyplot as plt
 
 
-H = 3
+H = 0.2
 RoomPoint = namedtuple('RoomPoint', ['x', 'y', 'room'])
 Point = namedtuple('Point', ['x', 'y'])
 Vector = namedtuple('Vector', ['x', 'y'])
@@ -23,8 +24,36 @@ def norm(v):
     l = length(v)
     return Vector(v.x / l, v.y / l)
 
+def point(r, p):
+    pnt = Point(r.p0.x + r.v.x * p, r.p0.y + r.v.y * p)
+    return Point(r.p0.x + r.v.x * p, r.p0.y + r.v.y * p)
+
+def intersection(r1, r2):
+    """
+    (x01, y01) = r1.p0
+    (a1, b1)   = r1.v
+
+    (x02, y02) = r2.p0
+    (a2, b2)   = r2.v
+    """
+
+    x01, y01 = r1.p0
+    a1, b1   = r1.v
+
+    x02, y02 = r2.p0
+    a2, b2   = r2.v
+
+    c1, c2 = x02 - x01, y02 - y01
+
+    A = np.matrix([[a1, a2], [b1, b2]])
+    b = np.matrix([[c1, c2]]).T
+    x = np.linalg.solve(A, b)
+
+    return Point(x01 + x[0,0] * a1, y01 + x[0,0] * b1)
+
 
 def calculate_suport_points(points):
+    sps = [[]]
     for ps in points:
         n = len(ps)
         for i in range(n):
@@ -43,15 +72,19 @@ def calculate_suport_points(points):
             # Director vector of second rect
             w = norm(vector(B, C))
 
-            # Rotate 90 grades
-            _v = Vector(v.y, v.x)
-            _w = Vector(w.y, w.x)
+            # Rotate -90 grades
+            _v = Vector(v.y, -v.x)
+            _w = Vector(w.y, -w.x)
 
-            _A = Point(xa + _v.x, ya + _v.y)
-            _B = Point(xb + _w.x, yb + _w.y)
+            _A = Point(xa + H * _v.x, ya + H * _v.y)
+            _B = Point(xb + H * _w.x, yb + H * _w.y)
 
             r1 = Rect(v, _A)
             r2 = Rect(w, _B)
+
+            sps[-1].append(intersection(r1, r2))
+        sps.append([])
+    return sps
 
 
 def get_all_points(rooms):
@@ -82,7 +115,18 @@ def generate(jsonfile):
         allps = get_all_points(rooms_map['rooms'])
         sallps = calculate_suport_points(allps)
 
-        #paint(allps)
+        for wps in allps:
+            x = [p.x for p in wps]
+            y = [p.y for p in wps]
+            plt.plot(x, y)
+
+        for wps in sallps:
+            x = [p.x for p in wps]
+            y = [p.y for p in wps]
+            plt.scatter(x, y)
+        plt.xlim([-1, 9.2])
+        plt.ylim([-1, 3.2])
+        plt.show()
 
 
 generate('map.json')
