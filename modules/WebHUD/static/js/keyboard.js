f0faeedccbb2755601ffdaf9b4cc430841fa1d69
Danilo Gómez Gómez <d.gomez@lab.matcom.uh.cu>
2016-03-16 10:49:32 -0400

    Make JS code `compatible` with more browsers

--- modules/WebHUD/static/js/keyboard.js
+++ modules/WebHUD/static/js/keyboard.js
@@ -1,5 +1,11 @@
 var pressed = new Set();
 
+function toArray(set){
+    array = [];
+    set.forEach(function(item){array.push(item)});
+    return array;
+}
+
 $(document).ready(function(){
   document.body.addEventListener('keydown', function(e) {
 
@@ -10,18 +16,18 @@ $(document).ready(function(){
 
     if ([87, 65, 83, 68].some(function(element, index, array) { return element === e.which })) {
       pressed.add(e.which);
-      sio.emit('manual', {'keys': [...pressed]})
+      sio.emit('manual', {'keys': toArray(pressed)})
     }
     
   }, true);
 
   document.body.addEventListener('keyup', function(e) {
     pressed.delete(e.which);
-    sio.emit('manual', {'keys': [...pressed]})
+    sio.emit('manual', {'keys': toArray(pressed)})
   }, true);
 /*
   setInterval(function(){
-    var l = [...pressed];
+    var l = toArray(pressed);
     sio.emit('manual', {'keys': l});
   }, 100);*/
 });
