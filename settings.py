3939163dc02f3b4653b9922f3cb3443dbbf3a175
Antonio Serrano Muñoz <toni.sm91@gmail.com>
2016-05-13 15:34:08 -0500

    Add log for the position of the robot

--- settings.py
+++ settings.py
@@ -7,8 +7,12 @@ __all__=['PROFILE', 'MODULES']
 
 import profiles
 
+
 # Name of the robot's profile directory (this directory is located in the folder: profiles)
 PROFILE = 'simubot'
 
 # Modules to load
 MODULES = profiles.WebHUD | profiles.ordex
+
+# Log
+LOG = False
