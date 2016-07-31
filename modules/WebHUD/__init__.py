2eb0be479ebb90a6d5687d350936deedad6858de
Rolando Urquiza <rolurquiza@gmail.com>
2016-07-31 16:05:12 -0400

    Add flask-static support

--- modules/WebHUD/__init__.py
+++ modules/WebHUD/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_socketio import SocketIO, emit
-
+from flask.ext.static import Static
 
 app = Flask(__name__)
+
 sio = SocketIO(app)
 sio.init_app(app)
+
+st = Static(app)
 
 if __name__ == '__main__':
     raise Exception('do not launch directly!')
