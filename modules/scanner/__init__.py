df3569f13af405540a341d8e27e59caa21a0ce64
Rolando Urquiza <rolurquiza@gmail.com>
2016-08-06 10:55:25 -0400

    Add module starter to scanner

--- modules/scanner/__init__.py
+++ modules/scanner/__init__.py
@@ -1,3 +1,17 @@
+from modules import kernel
 from scanner import Scanner
-from scanner import Cluster
-from scanner import Client
+
+
+scanner = None
+
+
+def run():
+    global scanner
+    settings = kernel.ROBOT.profile()
+    scanner = Scanner(scan=settings.get('SCANNER_SCAN', False),
+                      info=settings.get('SCANNER_INFO'),
+                      host=settings.get('SCANNER_HOST'))
+
+
+def stop():
+    scanner.stop()
