4c3e3f208dc1f3e03f6f33919d04f9a9e6910e6f
Rolando Urquiza <rolurquiza@gmail.com>
2016-08-06 20:36:36 -0400

    Fix WebHUD module import

--- modules/WebHUD/__init__.py
+++ modules/WebHUD/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask
-from flask_socketio import SocketIO, emit
 from flask.ext.static import Static
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 
 sio = SocketIO(app)
 sio.init_app(app)
 
-app.config['STATIC_RUN_ON_REFRESH'] = False
 st = Static(app)
+
+from . import restapi
+from . import views
+from . import static
 
 if __name__ == '__main__':
     raise Exception('do not launch directly!')
