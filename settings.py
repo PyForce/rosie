0cac1d54af23bd8d58bb64cfd495522d6884be6e
Rolando Urquiza <rolurquiza@gmail.com>
2016-08-06 20:41:09 -0400

    Create a settings object accepting a fallback value

--- settings.py
+++ settings.py
@@ -5,5 +5,31 @@ else:
     import ConfigParser as configparser
 
 
-config = configparser.ConfigParser(defaults={'active': False})
+class FallbackConfigParser(configparser.ConfigParser, object):
+    def get(self, section, option, fallback=None):
+        if self.has_section(section) and self.has_option(section, option):
+            return super(FallbackConfigParser, self).get(section, option)
+        else:
+            return fallback
+
+    def getint(self, section, option, fallback=None):
+        if self.has_section(section) and self.has_option(section, option):
+            return super(FallbackConfigParser, self).getint(section, option)
+        else:
+            return fallback
+
+    def getfloat(self, section, option, fallback=None):
+        if self.has_section(section) and self.has_option(section, option):
+            return super(FallbackConfigParser, self).getfloat(section, option)
+        else:
+            return fallback
+
+    def getboolean(self, section, option, fallback=None):
+        if self.has_section(section) and self.has_option(section, option):
+            return super(FallbackConfigParser, self).getboolean(section,
+                                                                option)
+        else:
+            return fallback
+
+config = FallbackConfigParser(defaults={'active': 'False', 'log': 'False'})
 config.read('config')
