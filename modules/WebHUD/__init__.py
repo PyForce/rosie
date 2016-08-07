18e8a52bb80f085a671f9608d519cb48cd2907bb
Rolando Urquiza <rolurquiza@gmail.com>
2016-08-06 20:39:06 -0400

    Add new module loading for WebHUD

--- modules/WebHUD/__init__.py
+++ modules/WebHUD/__init__.py
@@ -15,3 +15,18 @@ from . import static
 
 if __name__ == '__main__':
     raise Exception('do not launch directly!')
+
+
+def init():
+    from settings import config
+    host = config.get('WebHUD', 'host', '0.0.0.0')
+    port = config.getint('WebHUD', 'port', 5000)
+    debug = config.getboolean('WebHUD', 'debug', False)
+    use_reloader = config.getboolean('WebHUD', 'reloader', False)
+
+    sio.run(app, host=host, port=port, debug=debug, use_reloader=use_reloader)
+    #app.run(host=myhost, port=myport, debug=debug, use_reloader=use_reloader)
+
+
+def end():
+    sio.stop()
