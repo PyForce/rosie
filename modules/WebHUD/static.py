8a188b4253357cd8310de8fad6e32f7d4db61ac7
Rolando Urquiza <rolurquiza@gmail.com>
2016-09-19 23:14:29 -0400

    Add browserify extension and task

--- modules/WebHUD/static.py
+++ modules/WebHUD/static.py
@@ -1,4 +1,7 @@
+import subprocess
+
 from WebHUD import st
+from flask_gulp import extension
 
 
 @st.task('coffee')
@@ -7,7 +10,7 @@ def coffee_task():
         .pipe(coffee(bare=True, executable=
                      'node modules/WebHUD/node_modules/coffee-script/bin/'
                      'coffee'))\
-        .pipe(dest(output='modules/WebHUD/static/js/'))
+        .pipe(dest(output='modules/WebHUD/static/bundle/'))
 
 
 @st.task('cjsx')
@@ -15,7 +18,7 @@ def cjsx_task():
     src('static/coffee/**/*.cjsx')\
         .pipe(cjsx(bare=True, executable=
                    'node modules/WebHUD/node_modules/coffee-react/bin/cjsx'))\
-        .pipe(dest(output='modules/WebHUD/static/js/'))
+        .pipe(dest(output='modules/WebHUD/static/bundle/'))
 
 
 @st.task('less')
@@ -24,3 +27,29 @@ def less_task():
         .pipe(less(executable='node modules/WebHUD/node_modules/less/bin/'
                    'lessc'))\
         .pipe(dest(output='modules/WebHUD/static/css/'))
+
+
+@st.task('browserify')
+def browserify():
+    src('static/bundle/**/*.js')\
+        .pipe(browserify())\
+        .pipe(dest(output='modules/WebHUD/static/js/'))
+
+
+@extension
+def browserify(resources):
+    executable = browserify.settings.get('executable') or 'browserify'
+    bundle = browserify.settings.get('bundle') or 'bundle.js'
+
+    command = "%s " % executable
+    command += ' '.join((filename for filename, _ in resources))
+    command += ' -o'
+
+    process = subprocess.Popen(command, stdin=subprocess.PIPE,
+                               stdout=subprocess.PIPE, shell=True)
+    out, err = process.communicate()
+
+    if process.returncode:
+        yield None, err
+    else:
+        yield bundle, out
