a876232930dd5713846306c0b8ba4d2506553689
Rolando Urquiza <rolurquiza@gmail.com>
2017-02-05 08:16:38 -0500

    Remove view files from webhud module

--- modules/WebHUD/__init__.py
+++ modules/WebHUD/__init__.py
@@ -1,17 +1,14 @@
 from flask import Flask
-# from flask_gulp import Static
 from flask_sockets import Sockets
 from geventwebsocket import WebSocketServer
 from geventwebsocket.handler import WebSocketHandler
 
 app = Flask(__name__)
-# st = Static(app)
+
 ws = Sockets(app)
 server = None
 
 from . import restapi
-# from . import views
-# from . import static
 
 if __name__ == '__main__':
     raise Exception('do not launch directly!')
@@ -23,14 +20,6 @@ def init():
     port = config.getint('WebHUD', 'port', 5000)
     debug = config.getboolean('WebHUD', 'debug', False)
     app.debug = debug
-    app.config['STATIC_DEBUG'] = debug
-
-    # if debug:
-    #     st.watch(['static/coffee/**/*.coffee', 'static/coffee/**/*.cjsx',
-    #               'static/less/**/*.less'], 'cjsx', 'coffee', 'less',
-    #              'browserify')
-    # else:
-    #     st.runall()
 
     global server
     server = WebSocketServer((host, port), app, handler_class=WebSocketHandler)
