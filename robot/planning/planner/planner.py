de3ac437fa7e7d539c26184fa0020fe2888b9572
Fernando Martínez González <frndmartinezglez@gmail.com>
2017-02-23 07:10:36 -0500

    Implement list_maps in map_tools.py

--- robot/planning/planner/planner.py
+++ robot/planning/planner/planner.py
@@ -1,13 +1,15 @@
-import os
 import json
+import os
 
 from ..graph_extractor import Map
 
 
 class Planner(object):
+
     def __init__(self, mapdir=None):
         self.mapdir = mapdir or os.path.join(os.path.dirname(__file__), '..',
                                              'maps')
+        self.__map = {}
 
     def get_points(self, start, end):
         """
@@ -17,29 +19,23 @@ class Planner(object):
 
     @property
     def map(self):
-        return self._map
+        return self.__map
 
     @map.setter
     def map(self, value):
         # interpret string as map name
         if isinstance(value, str):
-            self._map = self.get_map(value)
+            self.__map = self.get_map(value)
         # receive dict for new map
         elif isinstance(value, dict):
-            self._map = value.copy()
+            self.__map = value.copy()
         # transform self._map in the corresponding graph
         self.graph = None
 
-    def maps(self, dir=None):
-        """
-            Iterator that yields map objects content
-        """
-        folder = dir or self.mapdir
-        for map in os.listdir(folder):
-            with open(os.path.join(folder, map), 'r') as map_file:
-                yield json.load(map_file)
+    def use_map(self, map_name):
+        self.__map = self.get_map(map_name)
 
-    def get_map(self, map_name, dir=None):
-        for map in self.maps(dir):
+    def get_map(self, map_name, mapdir=None):
+        for map in self.list_maps(mapdir):
             if map['name'] == map_name:
                 return map
